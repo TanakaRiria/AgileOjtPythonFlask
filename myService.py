140b2eede1c2ca867b1f75db72a58ef5c5ae6133
Kazz Nagashima <nagashima_kaz@texnos.co.jp>
2021-10-06 09:41:10 +0900

    API Pathを整備

--- myService.py
+++ myService.py
@@ -1,10 +1,14 @@
 from flask import Flask, jsonify
  
 app = Flask(__name__)
- 
+
 @app.route('/')
-def hello_world():
+def home():
+    return 'My Service API'
+
+@app.route('/api/v1/articles')
+def get_articles():
     return jsonify({'message': '"Congratulations, your Flask application is up and running"'})
- 
+
 if __name__ == "__main__":
     app.run(host='127.0.0.1', port=8090, debug=True)

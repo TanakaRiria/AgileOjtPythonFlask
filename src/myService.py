4c70276d4a8b0eb7241db5eea4ac497e501fea70
Kazz Nagashima <nagashima_kaz@texnos.co.jp>
2022-09-10 16:56:33 +0900

    host指定の修正

--- src/myService.py
+++ src/myService.py
@@ -11,4 +11,4 @@ def get_articles():
     return jsonify({'message': '"Congratulations, your Flask application is up and running"'})
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8090, debug=True)
+    app.run(host='0.0.0.0', port=8090, debug=True)
